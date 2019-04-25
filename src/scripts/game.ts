365d460e79f6ccae4a2ba87985f85c8dc0deab3a
Guy Bianco IV <gjbiancoiv@gmail.com>
2019-04-24 20:40:56 -0500

    format

--- src/scripts/game.ts
+++ src/scripts/game.ts
@@ -1,9 +1,9 @@
-import 'phaser'
-import MainScene from './scenes/mainScene'
-import PreloadScene from './scenes/preloadScene'
+import 'phaser';
+import MainScene from './scenes/mainScene';
+import PreloadScene from './scenes/preloadScene';
 
-const DEFAULT_WIDTH = 1280
-const DEFAULT_HEIGHT = 720
+const DEFAULT_WIDTH = 1280;
+const DEFAULT_HEIGHT = 720;
 
 const config: GameConfig = {
   backgroundColor: '#ffffff',
@@ -22,9 +22,9 @@ const config: GameConfig = {
       gravity: { y: 400 }
     }
   }
-}
+};
 
 window.addEventListener('load', () => {
-  let game = new Phaser.Game(config)
-})
+  let game = new Phaser.Game(config);
+});
 //
